a426aee894e8fd8f8bc98622826067c4e04edfc8
cinth <votacinthia@gmail.com>
2024-06-24 23:33:25 -0300

    Prueba comentario

--- app/src/main/java/com/example/clubdeportivo/ActivitiesActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/ActivitiesActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-
+//prueba comentario
 class ActivitiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
