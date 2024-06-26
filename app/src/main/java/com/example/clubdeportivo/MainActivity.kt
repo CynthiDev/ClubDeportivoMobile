b7d24e715158a7cde178da71ca74338dfb52c6f0
cinth <votacinthia@gmail.com>
2024-06-26 01:16:17 -0300

    Base de datos estructura + Datos precargados + Login funcionando

--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -5,18 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+import android.widget.EditText
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: Database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnIngreso: Button = findViewById(R.id.btnIngresar)
-        btnIngreso.setOnClickListener {
-            val intent = Intent(this,StartActivity::class.java)
-            startActivity(intent)
-        }
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+
+        val btnIngreso: Button = findViewById<Button>(R.id.btnIngresar)
+//        btnIngreso.setOnClickListener {
+//            val intent = Intent(this,StartActivity::class.java)
+//            startActivity(intent)
+//        }
         val btnSalir: Button = findViewById(R.id.btnSalir)
         btnSalir.setOnClickListener { finishAffinity() }
+
+        // Eliminar la base de datos
+        deleteDatabase("ClubDeportivo.db")
+
+        database = Database(this)
+
+        btnIngreso.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (database.validateAdmin(username, password)) {
+                val intent = Intent(this, StartActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 
